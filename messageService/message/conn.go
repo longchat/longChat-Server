b0f3908904917f0452f2d4704b48659a4b226904
longXboy <longxboyhi@gmail.com>
2016-07-27 22:21:20 +0800

    refactory the msg server

--- messageService/message/conn.go
+++ messageService/message/conn.go
@@ -1,33 +1,19 @@
 package message
 
 import (
-	"github.com/golang/protobuf/proto"
+	"sync"
+
 	"github.com/gorilla/websocket"
-	messagepb "github.com/longchat/longChat-Server/common/protoc"
 )
 
 type conn struct {
-	Id   uint32
-	ws   *websocket.Conn
-	send chan interface{}
+	Id       uint32
+	userIds  []int64
+	groupIds []int64
+	ws       *websocket.Conn
+	wLock    sync.Mutex
 }
 
 func (c *conn) readPump() {
-	uLock.RLock()
-	uLock.RUnlock()
-}
 
-func (c *conn) writePump() {
-	for {
-		select {
-		case w, isok := <-c.send:
-			writer, err := w.conn.ws.NextWriter(MessageReq)
-			bytes, err := proto.Marshal(w.message)
-			if err != nil {
-				continue
-			}
-			writer.Write(bytes)
-			writer.Close()
-		}
-	}
 }
--- messageService/message/hub.go
+++ messageService/message/hub.go
@@ -2,45 +2,137 @@ package message
 
 import (
 	"sync"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	messagepb "github.com/longchat/longChat-Server/common/protoc"
 )
 
 const (
-	MessageReq = 0
-	UserReq    = 1
-	GroupReq   = 2
+	TypeMessage = 0
+	TypeUser    = 1
+	TypeGroup   = 2
 )
 
 type worker struct {
-	isWorking  bool
-	lastWorkTs int64
-	writeCh    chan writeInfo
+	lastUseTs int64
+	ch        chan job
 }
 
 type workerPool struct {
-	workers     []worker
-	writeChPool sync.Pool
+	idle []worker
+	lock sync.Mutex
 }
 
-type connInfo struct {
-	userIds  []int64
-	groupIds []int64
-	conn     conn
-}
-
-type writeInfo struct {
+type job struct {
 	conn    *conn
 	message *messagepb.MessageReq
 }
 
 var (
-	uLock    *sync.RWMutex
-	cLock    *sync.RWMutex
-	gLock    *sync.RWMutex
-	userMap  map[int64]uint32
-	connMap  map[uint32]ConnInfo
-	groupMap map[int64]map[uint32]struct{}
+	userMap  map[int64]*conn
+	groupMap map[int64]map[uint32]*conn
 )
+
+var msgCh chan *messagepb.MessageReq
+
+func hub() {
+	var wp = workerPool{}
+	for {
+		select {
+		case msg := <-msgCh:
+			if len(msgCh) > 0 {
+				l := len(msgCh)
+				for i := 0; i < l; i++ {
+					msg2 := <-msgCh
+					msg.Messages = append(msg.Messages, msg2.Messages...)
+				}
+				var jobs map[uint32]job
+				jobs = make(map[uint32]job)
+				for i := range msg.Messages {
+					data := msg.Messages[i]
+					if data.IsGroupMessage {
+						group, isok := groupMap[data.To]
+						if isok {
+							for k, v := range group {
+								ajob, isok := jobs[k]
+								if isok {
+									ajob.message.Messages = append(ajob.message.Messages, data)
+								} else {
+									msgReq := messagepb.MessageReq{Messages: []*messagepb.MessageReq_Message{data}}
+									ajob = job{conn: v, message: &msgReq}
+								}
+								jobs[k] = ajob
+							}
+						}
+					} else {
+						userConn, isok := userMap[data.To]
+						if isok {
+							ajob, isok := jobs[userConn.Id]
+							if isok {
+								ajob.message.Messages = append(ajob.message.Messages, data)
+							} else {
+								msgReq := messagepb.MessageReq{Messages: []*messagepb.MessageReq_Message{data}}
+								ajob = job{conn: userConn, message: &msgReq}
+							}
+							jobs[userConn.Id] = ajob
+						}
+					}
+				}
+				for _, v := range jobs {
+					var aworker worker
+					wp.lock.Lock()
+					if len(wp.idle) > 0 {
+						aworker = wp.idle[len(wp.idle)-1]
+						wp.idle = wp.idle[:len(wp.idle)-1]
+					} else {
+						//todo:new worker
+					}
+					wp.lock.Unlock()
+					aworker.ch <- v
+				}
+			}
+
+		}
+	}
+}
 
 func controller() {
 
+}
+
+func (wp *workerPool) writePump(worker *worker) {
+	for {
+		worker.lastUseTs = time.Now().UnixNano()
+		wp.lock.Lock()
+		wp.idle = append(wp.idle, *worker)
+		wp.lock.Unlock()
+		select {
+		case info, isok := <-worker.ch:
+			if !isok {
+				break
+			}
+			writer, err := info.conn.ws.NextWriter(TypeMessage)
+			bytes, err := proto.Marshal(info.message)
+			if err != nil {
+				continue
+			}
+			id := info.conn.Id
+			info.conn.wLock.Lock()
+			if info.conn.Id != id {
+				info.conn.wLock.Unlock()
+				continue
+			}
+			_, err = writer.Write(bytes)
+			if err != nil {
+				info.conn.wLock.Unlock()
+				continue
+			}
+			err = writer.Close()
+			if err != nil {
+				info.conn.wLock.Unlock()
+				continue
+			}
+		}
+	}
 }
